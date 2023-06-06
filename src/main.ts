84bfb022a2f823413bce6fc036a8acc36a048142
AQian <970506352@qq.com>
2023-06-06 23:08:36 +0800

    feat: 引入pinia（追加）

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue"
+import { createPinia } from "pinia"
 import App from "./App.vue"
 import "virtual:uno.css"
 import "@unocss/reset/tailwind.css"
 import router from "./router"
 
-createApp(App).use(router).mount("#app")
+createApp(App).use(router).use(createPinia()).mount("#app")
