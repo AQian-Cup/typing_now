d4ac8c4ea7e7407f043be6dfdfa2a6815ace51c5
AQian <970506352@qq.com>
2023-06-08 23:14:01 +0800

    feat: 添加nextLine组件

---
+++ src/components/test/nextLine.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="flex justify-start px-1 w-full text-2xl text-blueGray-400">
+    {{ porps.text }}
+  </span>
+</template>
+<script lang="ts" setup>
+const porps = defineProps<{
+  text: string
+}>()
+</script>
+<style scoped>
+</style>
