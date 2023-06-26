b14e154f19e87905e0dea6900770f62e9907d6d8
AQian <970506352@qq.com>
2023-06-26 22:50:52 +0800

    feat: 引入languageSwitch组件，添加语言切换按钮

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="flex flex-col justify-center items-start p-10">
-    <button
-      v-for="button in homeButtons"
-      class="py-6 text-4xl"
-      @click="goPage(button.path)"
-    >
-      {{ button.name }}
-    </button>
+  <div class="flex justify-between p-10">
+    <div class="flex flex-col justify-center items-start">
+      <button
+        v-for="button in homeButtons"
+        class="py-6 text-4xl"
+        @click="goPage(button.path)"
+      >
+        {{ button.name }}
+      </button>
+    </div>
+    <NMessageProvider>
+      <LanguageSwitch></LanguageSwitch>
+    </NMessageProvider>
   </div>
 </template>
 <script lang="ts" setup>
+import { NMessageProvider } from "naive-ui"
 import { ref } from "vue"
 import { useRouter } from "vue-router"
+import LanguageSwitch from "@/components/home/languageSwitch.vue"
+
 const router = useRouter()
 const homeButtons = ref([
   { name: "打字测试", path: "/test" },
@@ -22,5 +30,4 @@ const goPage = (path: string) => {
   router.push(path)
 }
 </script>
-<style scoped>
-</style>
+<style scoped></style>
