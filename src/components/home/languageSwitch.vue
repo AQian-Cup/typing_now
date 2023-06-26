4d193d189dccc1362931797dd4f4036376a6a8c0
AQian <970506352@qq.com>
2023-06-26 22:50:04 +0800

    feat: 添加languageSwitch组件

---
+++ src/components/home/languageSwitch.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="i-majesticons-translate-line text-3xl" @click="info"></button>
+</template>
+<script lang="ts" setup>
+import { useMessage } from "naive-ui"
+
+const message = useMessage()
+const info = () => {
+  message.info("已切换语言", {
+    duration: 1000,
+    showIcon: false,
+  })
+}
+</script>
+<style scoped></style>
